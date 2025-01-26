86250f21fd28ca2bb53c8dba158e1af8b089fc03
vellimir <kkazancov@gmail.com>
2025-01-26 17:24:09 +0300

    Добавил редактирование пользователя и добавление изображения

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,7 @@
-from .models import CustomUser
 from rest_framework import serializers
+
+from .models import CustomUser
+from app.authentication.utils import *
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
@@ -9,5 +11,32 @@ class CurrentUserSerializer(serializers.ModelSerializer):
             'id',
             'username',
             'email',
+            'profile_image',
+        )
+
+class UpdateCurrentUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=False,
+        validators=[validate_username],
+    )
+    email = serializers.EmailField(
+        required=False,
+        validators=[validate_email],
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'id',
+            'username',
+            'email',
+        )
+
+class SetImageUserSerializer(serializers.ModelSerializer):
+    profile_image = serializers.ImageField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = (
             'profile_image',
         )
--- app/user/urls.py
+++ app/user/urls.py
@@ -7,5 +7,7 @@ from . import views
 from settings.env_config import SUFFIX_API
 
 urlpatterns: List[path] = [
-    path(f'{SUFFIX_API}user/', views.CurrentUserView.as_view(), )
+    path(f'{SUFFIX_API}user/', views.CurrentUserView.as_view(), ),
+    path(f'{SUFFIX_API}user/update', views.UpdateCurrentUserView.as_view(), ),
+    path(f'{SUFFIX_API}user/set-image', views.SetImageUserView.as_view(), )
 ]
