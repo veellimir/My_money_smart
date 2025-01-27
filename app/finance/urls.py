dff53d828dd0b4b1706c17bc23b177f0645435cb
vellimir <kkazancov@gmail.com>
2025-01-27 21:03:15 +0300

    Добавил удаления категорий

--- app/finance/urls.py
+++ app/finance/urls.py
@@ -6,6 +6,7 @@ from . import views
 from settings.settings import SuffixRouter
 
 urlpatterns: List[path] = [
+    path(f'{SuffixRouter.FINANCE}create-category', views.CreateCategoryView.as_view(), ),
+    path(f'{SuffixRouter.FINANCE}delete-category/<int:pk>', views.DeleteCategoryView.as_view(), ),
     path(f'{SuffixRouter.FINANCE}list-category', views.CategoryListView.as_view(), ),
-    path(f'{SuffixRouter.FINANCE}create-category', views.CreateCategoryView.as_view(), )
 ]
--- app/finance/views.py
+++ app/finance/views.py
@@ -6,6 +6,7 @@ from .serializers import (
     CreateCategorySerializer,
     CategoryListSerializer
 )
+from .exceptions import EXCEPTION_CATEGORY_NOT_FOUND
 
 
 class CreateCategoryView(generics.CreateAPIView):
@@ -13,6 +14,19 @@ class CreateCategoryView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class DeleteCategoryView(generics.DestroyAPIView):
+    serializer_class = CategoryListSerializer
+    queryset = MyCategory.objects.all()
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        category = MyCategory.objects.filter(pk=pk, user=self.request.user).first()
+
+        if not category:
+            raise EXCEPTION_CATEGORY_NOT_FOUND
+        return category
 
 
 class CategoryListView(BasedList):
