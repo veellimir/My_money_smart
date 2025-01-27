a2860b47130acdce680cef342db4118352ce176f
vellimir <kkazancov@gmail.com>
2025-01-27 18:57:24 +0300

    Создал приложение финансы

---
+++ app/finance/urls.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from django.urls import path
+
+
+from . import views
+from settings.env_config import SUFFIX_API
+from utils.suffix_router import SuffixRouter
+
+urlpatterns: List[path] = [
+
+]
--- settings/settings.py
+++ settings/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
     'app.authentication.apps.AuthenticationConfig',
     'app.user.apps.UserConfig',
+    'app.finance.apps.FinanceConfig',
 ]
 
 MIDDLEWARE = [
